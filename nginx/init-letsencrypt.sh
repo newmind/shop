306888abeab428dbc98c34966238ba4b608ec355
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-10 21:40:35 +0300

    nginx

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -70,29 +70,29 @@ case "$email" in
 esac
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
-for domain in ${!domains[*]}; do
-  domain_set=(${domains[$domain]})
-  domain_name=`echo ${domain_set[0]} | grep -o -P $regex`
-
-  if [ -e "$data_path/conf/live/$domain_name/cert.pem" ]; then
-    echo "Skipping $domain_name domain"; else
-
-    echo "### Deleting dummy certificate for $domain_name domain ..."
-    rm -rf "$data_path/conf/live/$domain_name"
-
-
-    echo "### Requesting Let's Encrypt certificate for $domain_name domain ..."
-
-    #Join $domains to -d args
-    domain_args=""
-    for domain in "${domain_set[@]}"; do
-      domain_args="$domain_args -d $domain"
-    done
-
-    mkdir -p "$data_path/www"
-    docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot --cert-name $domain_name $domain_args \
-    $staging_arg $email_arg --rsa-key-size $rsa_key_size --agree-tos --force-renewal --non-interactive" certbot
-  fi
-done
+#if [ $staging != "0" ]; then staging_arg="--staging"; fi
+#
+#for domain in ${!domains[*]}; do
+#  domain_set=(${domains[$domain]})
+#  domain_name=`echo ${domain_set[0]} | grep -o -P $regex`
+#
+#  if [ -e "$data_path/conf/live/$domain_name/cert.pem" ]; then
+#    echo "Skipping $domain_name domain"; else
+#
+#    echo "### Deleting dummy certificate for $domain_name domain ..."
+#    rm -rf "$data_path/conf/live/$domain_name"
+#
+#
+#    echo "### Requesting Let's Encrypt certificate for $domain_name domain ..."
+#
+#    #Join $domains to -d args
+#    domain_args=""
+#    for domain in "${domain_set[@]}"; do
+#      domain_args="$domain_args -d $domain"
+#    done
+#
+#    mkdir -p "$data_path/www"
+#    docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot --cert-name $domain_name $domain_args \
+#    $staging_arg $email_arg --rsa-key-size $rsa_key_size --agree-tos --force-renewal --non-interactive" certbot
+#  fi
+#done
