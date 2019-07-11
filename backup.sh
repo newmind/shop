8f71e2d31c945870ee76c67afbe215000b5080e3
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 00:09:00 +0300

    deploy

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@
 docker exec postgre_sql pg_dump --column-inserts --data-only products > /shop/src/product-proxy/backup/products.sql
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-docker exec postgre_sql pg_dump --column-inserts --data-only products > /shop/src/operation-proxy/backup/products.sql
+docker exec postgre_sql pg_dump --column-inserts --data-only operations > /shop/src/operation-proxy/backup/operations.sql
