e488f0f97f8b0e813aef68f5a99d354c8a543793
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 00:12:27 +0300

    deploy

--- backup.sh
+++ backup.sh
@@ -3,5 +3,8 @@
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
 docker exec postgre_sql pg_dump --column-inserts --data-only products > /shop/src/product-proxy/backup/products.sql
 
+[ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
+docker exec postgre_sql pg_dump --column-inserts --data-only identity > /shop/src/identity-srv/backup/identity.sql
+
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
 docker exec postgre_sql pg_dump --column-inserts --data-only operations > /shop/src/operation-proxy/backup/operations.sql
