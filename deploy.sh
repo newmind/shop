cfd1c8564b73a138523d3e5dfb9d50cf01f10337
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-11 23:32:01 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[ -d /shop/src/products/backup ] || mkdir /shop/src/products/backup
+
 docker exec postgre_sql pg_dump --column-inserts --data-only  products > /shop/src/products/backup/products.sql
 
 docker-compose up --build -d
