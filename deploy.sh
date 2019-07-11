9d73fc64ab32df28635beab290cd12ea3f06ba23
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-11 23:57:40 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose up --build -d
+[-d /shop/src/product-proxy/backup] || mkdir -p /shop/src/product-proxy/backup
+docker exec postgre_sql pg_dump --column-inserts --data-only  products > /shop/src/product-proxy/backup/products.sql
+
+[-d /shop/src/operation-proxy/backup] || mkdir -p /shop/src/product-proxy/backup
+docker exec postgre_sql pg_dump --column-inserts --data-only  products > /shop/src/operation-proxy/backup/products.sql
