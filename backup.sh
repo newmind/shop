00fe8b04490ff835601dd418cae7488979f4281f
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 01:50:19 +0300

    deploy

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-docker exec postgre_sql pg_dump -U admin -W 1Ctdfcnjgjkm -f /shop/src/product-proxy/backup/products.sql products
+docker exec postgre_sql pg_dump -U admin -W 1Ctdfcnjgjkm -f /shop/src/product-proxy/backup/products.sql -Fd products
 
 [ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
-docker exec postgre_sql pg_dump -U admin -W 1Ctdfcnjgjkm -f /shop/src/identity-srv/backup/identity.sql identity
+docker exec postgre_sql pg_dump -U admin -W 1Ctdfcnjgjkm -f /shop/src/identity-srv/backup/identity.sql -Fd identity
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/operation-proxy/backup
-docker exec postgre_sql pg_dump -U admin -W 1Ctdfcnjgjkm -f /shop/src/operation-proxy/backup/operations.sql operations
+docker exec postgre_sql pg_dump -U admin -W 1Ctdfcnjgjkm -f /shop/src/operation-proxy/backup/operations.sql -Fd operations
