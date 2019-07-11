933a2b10b49da7e2b66871ccdb970a81c80b3fc7
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 00:58:52 +0300

    deploy

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-docker exec postgre_sql pg_dump -Fc products > /shop/src/product-proxy/backup/products.sql
+docker exec postgre_sql pg_dump -Fc products /shop/src/product-proxy/backup/products.sql
 
 [ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
-docker exec postgre_sql pg_dump -Fc identity > /shop/src/identity-srv/backup/identity.sql
+docker exec postgre_sql pg_dump -Fc identity /shop/src/identity-srv/backup/identity.sql
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-docker exec postgre_sql pg_dump -Fc operations > /shop/src/operation-proxy/backup/operations.sql
+docker exec postgre_sql pg_dump -Fc operations /shop/src/operation-proxy/backup/operations.sql
