4028f97a54db589baf015fa6d0cb11ecdcab4cff
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-13 00:30:46 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -5,7 +5,9 @@ echo "|   BACKUP PRODUCTS   |"
 echo "-----------------------"
 
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-docker exec postgre_sql pg_dump -Fc -U postgres products > /shop/src/product-proxy/backup/dump.bak
+#docker exec postgre_sql pg_dump -Fc -U postgres products > /shop/src/product-proxy/backup/dump.bak
+docker exec postgre_sql pg_dump --username "postgres" --no-password --verbose --role "admin" --format=c --blobs --data-only --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --encoding "UTF8" "products" > "/shop/src/product-proxy/backup/dump.bak"
+
 
 echo "-----------------------"
 echo "|   BACKUP IDENTITY   |"
@@ -20,4 +22,5 @@ echo "|   BACKUP OPERATIONS   |"
 echo "-------------------------"
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/operation-proxy/backup
-docker exec postgre_sql pg_dump -Fc -U postgres operations > /shop/src/operation-proxy/backup/dump.bak
+#docker exec postgre_sql pg_dump -Fc -U postgres operations > /shop/src/operation-proxy/backup/dump.bak
+docker exec postgre_sql pg_dump --username "postgres" --no-password --verbose --role "admin" --format=c --blobs --data-only --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --encoding "UTF8" "operations" > "/shop/src/operation-proxy/backup/dump.bak"
