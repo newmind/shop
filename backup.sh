e1293c43800a1d1728965acf8806e683c0ef4edd
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 23:38:03 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -5,18 +5,18 @@ echo "|   BACKUP PRODUCTS   |"
 echo "-----------------------"
 
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-docker exec postgre_sql pg_dump -U postgres products > /shop/src/product-proxy/backup/dump.sql
+docker exec postgre_sql pg_dump -Fc -U postgres products > /shop/src/product-proxy/backup/dump.bak
 
 echo "-----------------------"
 echo "|   BACKUP IDENTITY   |"
 echo "-----------------------"
 
 [ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
-docker exec postgre_sql pg_dump -U postgres identity > /shop/src/identity-srv/backup/dump.sql
+docker exec postgre_sql pg_dump -Fc -U postgres identity > /shop/src/identity-srv/backup/dump.bak
 
 echo "-------------------------"
 echo "|   BACKUP OPERATIONS   |"
 echo "-------------------------"
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/operation-proxy/backup
-docker exec postgre_sql pg_dump -U postgres operations > /shop/src/operation-proxy/backup/dump.sql
+docker exec postgre_sql pg_dump -Fc -U postgres operations > /shop/src/operation-proxy/backup/dump.bak
