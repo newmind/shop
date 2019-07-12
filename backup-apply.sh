30940df308705c6ca5a986e0ab1eea9816637aa0
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 23:40:44 +0300

    backup

--- backup-apply.sh
+++ backup-apply.sh
@@ -4,16 +4,16 @@ echo "-----------------------"
 echo "|   BACKUP PRODUCTS   |"
 echo "-----------------------"
 
-docker exec -i postgre_sql psql -Fc -U postgres -d products < /shop/src/product-proxy/backup/dump.sql
+docker exec -i postgre_sql psql -Fc -U postgres -d products < /shop/src/product-proxy/backup/dump.bak
 
 echo "-----------------------"
 echo "|   BACKUP IDENTITY   |"
 echo "-----------------------"
 
-docker exec -i postgre_sql psql -Fc -U postgres -d identity < /shop/src/identity-srv/backup/dump.sql
+docker exec -i postgre_sql psql -Fc -U postgres -d identity < /shop/src/identity-srv/backup/dump.bak
 
 echo "-------------------------"
 echo "|   BACKUP OPERATIONS   |"
 echo "-------------------------"
 
-docker exec -i postgre_sql psql -Fc -U postgres -d operations < /shop/src/operation-proxy/backup/dump.sql
+docker exec -i postgre_sql psql -Fc -U postgres -d operations < /shop/src/operation-proxy/backup/dump.bak
