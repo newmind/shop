8d2825d4adabcf1a167e730facf6860a11b39ef7
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-13 00:49:38 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -5,7 +5,6 @@ echo "|   BACKUP PRODUCTS   |"
 echo "-----------------------"
 
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
-#docker exec postgre_sql pg_dump -Fc -U postgres products > /shop/src/product-proxy/backup/dump.bak
 docker exec postgre_sql pg_dump --username "postgres" --no-password --verbose --role "admin" --format=c --blobs --data-only --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --encoding "UTF8" "products" > "/shop/src/product-proxy/backup/dump.bak"
 
 
@@ -14,7 +13,6 @@ echo "|   BACKUP IDENTITY   |"
 echo "-----------------------"
 
 [ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
-#docker exec postgre_sql pg_dump -Fc -U postgres identity > /shop/src/identity-srv/backup/dump.bak
 docker exec postgre_sql pg_dump --username "postgres" --no-password --verbose --role "admin" --format=c --blobs --data-only --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --encoding "UTF8" "identity" > "/shop/src/identity-srv/backup/dump.bak"
 
 echo "-------------------------"
@@ -22,5 +20,4 @@ echo "|   BACKUP OPERATIONS   |"
 echo "-------------------------"
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/operation-proxy/backup
-#docker exec postgre_sql pg_dump -Fc -U postgres operations > /shop/src/operation-proxy/backup/dump.bak
 docker exec postgre_sql pg_dump --username "postgres" --no-password --verbose --role "admin" --format=c --blobs --data-only --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --encoding "UTF8" "operations" > "/shop/src/operation-proxy/backup/dump.bak"
