f1d1a3cefcc9de149c9cef323e76117284577142
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-13 00:26:24 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -12,7 +12,8 @@ echo "|   BACKUP IDENTITY   |"
 echo "-----------------------"
 
 [ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
-docker exec postgre_sql pg_dump -Fc -U postgres identity > /shop/src/identity-srv/backup/dump.bak
+#docker exec postgre_sql pg_dump -Fc -U postgres identity > /shop/src/identity-srv/backup/dump.bak
+docker exec postgre_sql pg_dump --file "/shop/src/identity-srv/backup/dump.bak" --username "postgres" --no-password --verbose --role "admin" --format=c --blobs --data-only --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --encoding "UTF8" "identity"
 
 echo "-------------------------"
 echo "|   BACKUP OPERATIONS   |"
