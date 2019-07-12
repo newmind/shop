84445179a917b5b32d7bb3bd690d71544a9cb610
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 23:26:38 +0300

    backup

--- backup-apply.sh
+++ backup-apply.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-ll /shop/src/product-proxy/backup
+echo "-----------------------"
+echo "|   BACKUP PRODUCTS   |"
+echo "-----------------------"
 
 docker exec -i postgre_sql psql -U postgres -d products < /shop/src/product-proxy/backup/dump.sql
 
+echo "-----------------------"
+echo "|   BACKUP IDENTITY   |"
+echo "-----------------------"
+
 docker exec -i postgre_sql psql -U postgres -d identity < /shop/src/identity-srv/backup/dump.sql
+
+echo "-------------------------"
+echo "|   BACKUP OPERATIONS   |"
+echo "-------------------------"
 
 docker exec -i postgre_sql psql -U postgres -d operations < /shop/src/operation-proxy/backup/dump.sql
--- backup.sh
+++ backup.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+echo "-----------------------"
+echo "|   BACKUP PRODUCTS   |"
+echo "-----------------------"
+
 [ -d /shop/src/product-proxy/backup ] || mkdir -p /shop/src/product-proxy/backup
 docker exec postgre_sql pg_dump -U postgres products > /shop/src/product-proxy/backup/dump.sql
 
+echo "-----------------------"
+echo "|   BACKUP IDENTITY   |"
+echo "-----------------------"
+
 [ -d /shop/src/identity-srv/backup ] || mkdir -p /shop/src/identity-srv/backup
 docker exec postgre_sql pg_dump -U postgres identity > /shop/src/identity-srv/backup/dump.sql
+
+echo "-------------------------"
+echo "|   BACKUP OPERATIONS   |"
+echo "-------------------------"
 
 [ -d /shop/src/operation-proxy/backup ] || mkdir -p /shop/src/operation-proxy/backup
 docker exec postgre_sql pg_dump -U postgres operations > /shop/src/operation-proxy/backup/dump.sql
