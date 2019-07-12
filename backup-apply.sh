8237835969ca59ce8d291c89ed17c4f4b6e39083
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 21:14:58 +0300

    deploy

--- backup-apply.sh
+++ backup-apply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[ -f /shop/src/product-proxy/backup/products.sql ] && docker exec postgre_sql zcat /shop/src/product-proxy/backup/dump.gz | psql products
+[ -f /shop/src/product-proxy/backup/dump.gz ] && docker exec postgre_sql zcat /shop/src/product-proxy/backup/dump.gz | psql products
 
-[ -f /shop/src/identity-srv/backup/identity.sql ] && docker exec postgre_sql zcat /shop/src/identity-srv/backup/dump.gz | psql identity
+[ -f /shop/src/identity-srv/backup/dump.gz ] && docker exec postgre_sql zcat /shop/src/identity-srv/backup/dump.gz | psql identity
 
-[ -f /shop/src/operation-proxy/backup/operations.sql ] && docker exec postgre_sql zcat /shop/src/operation-proxy/backup/dump.gz | psql operations
+[ -f /shop/src/operation-proxy/backup/dump.gz ] && docker exec postgre_sql zcat /shop/src/operation-proxy/backup/dump.gz | psql operations
