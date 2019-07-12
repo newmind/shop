4b53f3ac0c0f7bfc5481a35d7a381be64222e69a
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-07-12 21:58:44 +0300

    operation proxy

--- backup-apply.sh
+++ backup-apply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[ -f /shop/src/product-proxy/backup/dump.gz ] || docker exec postgre_sql pg_restore -d products /shop/src/product-proxy/backup/dump.gz
+docker exec postgre_sql pg_restore -d products /shop/src/product-proxy/backup/dump.gz
 
-[ -f /shop/src/identity-srv/backup/dump.gz ] || docker exec postgre_sql pg_restore -d identity /shop/src/identity-srv/backup/dump.gz
+docker exec postgre_sql pg_restore -d identity /shop/src/identity-srv/backup/dump.gz
 
-[ -f /shop/src/operation-proxy/backup/dump.gz ] || docker exec postgre_sql pg_restore -d operations /shop/src/operation-proxy/backup/dump.gz
+docker exec postgre_sql pg_restore -d operations /shop/src/operation-proxy/backup/dump.gz
