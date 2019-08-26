8ecac95f40c93af59f216f16dd7fa0925602b01d
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-08-26 21:24:30 +0300

    prerender

--- src/prerender-srv/src/index.mjs
+++ src/prerender-srv/src/index.mjs
@@ -1,54 +1,61 @@
+
+import http from 'http';
+import Koa from 'koa';
+
 import chromeLauncher from 'chrome-launcher';
 import CDP from 'chrome-remote-interface';
-import file from 'fs';
+
+
+async function launchChrome() {
+  return await chromeLauncher.launch({
+    chromeFlags: [
+      '--disable-gpu',
+      '--headless'
+    ]
+  });
+}
+
+function transform(html) {
+  return html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+}
 
 
 (async function() {
-  async function launchChrome() {
-    return await chromeLauncher.launch({
-      chromeFlags: [
-        '--disable-gpu',
-        '--headless'
-      ]
-    });
-  }
+
+  const app = new Koa();
+  const server = http.createServer(app.callback());
+
   const chrome = await launchChrome();
   const protocol = await CDP({
     port: chrome.port
   });
 
-  const {
-    DOM,
-    Page,
-    Emulation,
-    Runtime
-  } = protocol;
+  const { DOM, Page, Runtime } = protocol;
 
   await Promise.all([Page.enable(), Runtime.enable(), DOM.enable()]);
 
-  Page.navigate({
-    url: 'https://магазиночков.рф/products'
+  app.use(async (ctx) => {
+
+    Page.navigate({ url: 'https://магазиночков.рф' + ctx.request.url });
+
+    ctx.body = await new Promise((resolve) => {
+
+      Page.loadEventFired(async() => {
+
+        const result = await Runtime.evaluate({
+          expression: `new Promise((fulfill, reject) => setTimeout(() => fulfill(document.documentElement.outerHTML), 1000))`,
+          awaitPromise: true
+        });
+
+        // protocol.close();
+        // chrome.kill();
+
+        resolve(transform(result.result.value));
+      });
+    });
   });
 
-  Page.loadEventFired(async() => {
-    const result = await Runtime.evaluate({
-      expression: `new Promise((fulfill, reject) => setTimeout(() => fulfill(document.documentElement.outerHTML), 1000))`,
-      awaitPromise: true
-    });
-    console.log(result.result.value)
-    // const ss = await Page.getFrameTree({format: 'mhtml', fromSurface: true});
-    //
-    // console.log(ss);
-    //
-    file.writeFile('./screenshot.html', result.result.value, 'utf8', function(err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-
-    protocol.close();
-    chrome.kill();
+  server.listen(5040, () => {
+    console.log('server start');
   });
-
-  // ЗДЕСЬ ВСЕ ПОСЛЕДУЮЩИЕ ПРИМЕРЫ КОДА
 })();
