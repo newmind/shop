3bdfe7387c9b7a569313fb0841ce701e2d220033
Viktor Pyatakov <viktor.pyatakov@pimpay.ru>
2019-09-09 22:27:43 +0300

    db migration and seed

--- src/operation-proxy/db/config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  host: process.env.DATA_BASE_HOST,
-  database: process.env.DATA_BASE_NAME,
-  username: process.env.DATA_BASE_USERNAME,
-  password: process.env.DATA_BASE_PASSWORD,
-  dialect: 'postgres'
-};
