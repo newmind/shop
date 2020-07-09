39d0e88941c74ecf4b1ea9101cabc6e2bbc99a99
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2020-07-09 13:13:53 +0300

    produce

--- shop.modules.packages/produce/src/components/Pay/index.js
+++ shop.modules.packages/produce/src/components/Pay/index.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 
+import cn from 'classnames';
 import styles from './default.module.scss';
 
 
@@ -11,7 +12,22 @@ export default () => {
         <h2>Оплата</h2>
       </header>
       <article className={styles['content']}>
-
+        <div className={styles['block']}>
+          <div className={styles['block__col']}>
+            <div className={styles['pay']}>
+              <span className={cn(styles['pay__icon'], 'fas fa-wallet')} />
+              <p className={styles['pay__label']}>Наличными при получении</p>
+              <p className={styles['pay__description']}>Возможность произвести оплату при получении товара. Оплата производится наличными или банковской картой</p>
+            </div>
+          </div>
+          <div className={styles['block__col']}>
+            <div className={styles['pay']}>
+              <span className={cn(styles['pay__icon'], 'far fa-credit-card')} />
+              <p className={styles['pay__label']}>Онлайн оплата</p>
+              <p className={styles['pay__description']}>Оплата онлайн производится до оформления и принятия заказа в работу. Оплата производится только банковской картой</p>
+            </div>
+          </div>
+        </div>
       </article>
     </section>
   );
