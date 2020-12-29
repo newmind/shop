291cd9ee1e74eb4ac0cd661d3906a3495ba5298f
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2020-12-29 19:41:42 +0300

    reformat

--- apps/admin-ui/src/wrappers/Empty/Header/Component.js
+++ apps/admin-ui/src/wrappers/Empty/Header/Component.js
@@ -1,27 +1,18 @@
 
-import { Cart } from '@ui.packages/cart';
-
-import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 import styles from './default.module.scss';
 
 
-class Component extends PureComponent {
-  render() {
-    return (
-      <div className={styles['header']}>
-        <div className={styles['header__title']}>
-          <div className={styles['container']}>
-            <Link className={styles['logotype']} to={'/'}><i className="fas fa-glasses" />&nbsp;&nbsp;&nbsp;Магазин очков</Link>
-          </div>
-        </div>
-        <div className={styles['header__cart']}>
-          <Cart />
+export default function Header() {
+  return (
+    <div className={styles['header']}>
+      <div className={styles['header__title']}>
+        <div className={styles['container']}>
+          <Link className={styles['logotype']} to={'/'}><i className="fas fa-glasses" />&nbsp;&nbsp;&nbsp;Магазин очков</Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Component;
