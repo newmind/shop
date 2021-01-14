0565309edc7b1af4bad38b198b9e6eaeae707772
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-01-15 01:59:37 +0300

    design

--- modules/produce/src/components/index.js
+++ modules/produce/src/components/index.js
@@ -1,16 +1,12 @@
 
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 import Component from './Component';
-
-import { pageInProcess } from '../ducks/commands';
 
 
 const mapStateToProps = () => ({});
 
-const mapActionsToProps = (dispatch) => ({
-  pageInProcess: bindActionCreators(pageInProcess, dispatch),
+const mapActionsToProps = () => ({
 });
 
 
